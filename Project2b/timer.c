3dd17a487aa794152bfb69931d5c3311866bd7bd
kjd2060 <gamelord317@gmail.com>
2017-12-04 15:14:35 -0500

    added some comments to make it make more sense

--- Project2b/timer.c
+++ Project2b/timer.c
@@ -35,13 +35,17 @@ void * pwmThread(void *args){
  */ 
 void pwmInit(){
     PWMArgs pwmArgs;
+    
+    // see http://www.qnx.com/developers/docs/6.5.0/index.jsp?topic=%2Fcom.qnx.doc.neutrino_lib_ref%2Ft%2Fthreadctl.html
+    // allows the thread to access lower level I/O and memory privledges
     ThreadCtl(_NTO_TCTL_IO, NULL);
-    uintptr_t ctrl = mmap_device_io(8, BASE + 10);
+    
+    uintptr_t ctrl = mmap_device_io(8, BASE + 10); // use Digital I/O port C
     if(ctrlHandle == MAP_DEVICE_FAILED)
     {
         perror("map control register fails");
     }
-    out8(ctrl, 0x00 );
+    out8(ctrl, 0x00 );                             // write an 8 bit value to port C of all 0s
     for (j = 0; j < NUMOFPWM; j++) {
         pwmArgs.arg1 = j;
         pthread_create (NULL, NULL, pwmThreadFunc, (void *)&pwmArgs);
